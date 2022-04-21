005c9c95374ffc3f5a89aae51d110b3518dc2c7c
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-04-21 19:59:37 +0300

    assessment

--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,20 @@
 
 
 fun main() {
-    var account = CurrentAccount(758862979, "OchwadaWalala", 56000)
+    var account = CurrentAccount(758862979, "OchwadaWalala", 56000.50)
     account.deposit(100.00)
-    println(account.balance)
-    account.deposit(200.50)
-    println(account.balance)
+    account.withdraw(200.50)
     account.details()
-    names()
+    var savings = SavingsAccount(3,758862979,"OchwadaWalala",56000.50)
+    savings.withdraw(200.50)
+
+    var products = Product("Kleesoft",5,140.50,"Hygiene")
+    println(products)
+
+    var stmt = words("Sugarcane")
+    println(stmt)
+
+
 
 
 
@@ -22,24 +29,22 @@ fun main() {
 //c.details() - Prints out the account number and balance and
 //name in this format: “Account number x with balance y is
 //operated by z
-open class CurrentAccount(var accountNumber: Int, var accountName: String, var balance: Int) {
-    fun deposit(amount: Double){
-       balance+=amount
-        return balance
+open class CurrentAccount(var accountNumber: Int, var accountName: String, var balance: Double) {
+    fun deposit(amount: Double) {
+        balance += amount
+        println(balance)
     }
 
-   open fun withdraw(amount: Double) {
-        balance-=amount
-        return balance
+    open fun withdraw(amount: Double) {
+        balance -= amount
+        println(balance)
 
+    }
+    fun details(){
+        println("Account number $accountNumber with balance $balance is operated by $accountName")
+    }
 }
-
-fun details() {
-    println("Account number $accountNumber with balance $balance is operated by $accountName")
-}
-
-}
-//Create another classSavingsAccount. It has the same functions and
+//Create another class SavingsAccount. It has the same functions and
 //attributes as the current account except for one attribute,
 //withdrawals: Int. Withdrawals is a counter variable that is used to
 //keep track of how many withdrawals have been made from the account
@@ -47,33 +52,42 @@ fun details() {
 //withdraw() method first checks if the number of withdrawals is less
 //than 4 for it to allow one to withdraw money from the account. It also
 //increments the withdrawals attribute after a successful withdrawal
-class SavingsAccount(accountNumber: Int, accountName: String, balance: Int):CurrentAccount(accountNumber, accountName, balance){
-   fun withdrawals():Int{
-
-   }
-
+class SavingsAccount(var withdrawals:Int, accountNumber: Int, accountName: String, balance: Double): CurrentAccount(accountNumber, accountName, balance){
     override fun withdraw(amount: Double) {
-       // super.withdraw(amount)
-       if (withdrawals(<4){
-           println()
+        if (withdrawals<4){
+            super.withdraw(amount)
+            withdrawals++
+            println("Can withdraw money")
         }
+        else{
+            println("Cannot withdraw")
+        }
+
     }
 }
 //A product is represented by a data class with these attributes: name,
 //weight, price, category. Category can either be groceries, hygiene or
 //other. Write a function that takes in a product and uses a when
 //statement to assign each product to a list based on its category
-data class (var name:String, var weight:Int, var price:Int, var category:String){
-    fun groceries(){
-
+data class Product(var name:String,var weight:Int,var price:Double,var category:String)
+fun categorizedproduct(product:Product){
+    var groceriesList= mutableListOf<Product>()
+    var hygieneList= mutableListOf<Product>()
+    var otherList= mutableListOf<Product>()
+    when(product.category){
+        "groceries"->groceriesList.add(product)
+        "hygiene"->hygieneList.add(product)
+        else->otherList.add(product)
     }
-
+    println(groceriesList)
+    println(hygieneList)
+    println(otherList)
 }
+
 //Write a function that given a string returns a string composed of only
 //the characters in even indices. For example given “banana” it will return
 //“bnn”
-fun names(){
-    var x = "Pineapple"
-    println()
-
-}
+ fun words(word: String): String{
+    var k= word.filterIndexed { index, name -> index%2==0 }
+   return k
+ }
